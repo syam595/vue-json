7a8a02b0087a946fefe4683aaca2c61cfd48216e
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2020-04-19 11:41:51 +0200

    Basic json operations in Vue
    Basic json operations in Vue

---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+    <div>
+<h2>Addresses </h2>
+<ul id="menu-standard">
+    <li id="menu-standard" v-for="item in myJson.addresses" v-bind:key="item" >
+      <div  v-for="(value, key) in item" v-bind:key="key">
+        <tr><td> <strong>{{key}} : </strong></td><td> {{value}} </td></tr>
+      </div>
+    </li>
+  </ul>
+
+<!--
+<h2>Transaction Details </h2>
+<ul id="menu-standard">
+    <li id="menu-standard" v-for="item in myJson.paymentMethods" v-bind:key="item" >
+      <div  v-for="(value, key) in item" v-bind:key="key">
+        <tr><td> <strong>{{key}} : </strong></td><td> {{value}} </td></tr>
+      </div>
+    </li>
+  </ul> -->
+
+  {{ keys }}
+    </div>
+</div>
+  
+</template>
+
+<script>
+import json from './test.json'
+import djson from './test_expectedAddressJson.json'
+//import axios from "axios";
+
+
+export default {
+  name: 'Home',
+  props: {
+   // msg: String,
+    //myJson: json
+  },
+  data(){
+      return{
+          myJson: json,
+          metric: null,
+          addresses: null,
+          myAdd: djson,
+          //hson
+      }
+  }
+  
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#menu-standard { 
+    float: left;
+}
+
+
+</style>
